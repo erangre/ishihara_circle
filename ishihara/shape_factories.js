0c8a4be6013e845325f935c1724d1bc4305e2d08
Eran Greenberg <erangre@gmail.com>
2023-12-27 17:10:05 +0100

    Update shape_factories.js
    changed to b>127

--- ishihara/shape_factories.js
+++ ishihara/shape_factories.js
@@ -39,7 +39,7 @@ CircleFactory.prototype.overlaps_image = function(img_data, circle) {
       var b = img_data.data[index + 2];
       var a = img_data.data[index + 3];
       debugger
-      if ((r+g) * (a / 255) < 127) {
+      if (b>127) {
         points_overlapping++;
       }
     }
