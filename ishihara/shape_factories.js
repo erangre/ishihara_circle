209df87491e74b2254a616a4bc68400a03992828
Eran Greenberg <erangre@gmail.com>
2023-12-27 17:36:42 +0100

    Update shape_factories.js

--- ishihara/shape_factories.js
+++ ishihara/shape_factories.js
@@ -39,7 +39,7 @@ CircleFactory.prototype.overlaps_image = function(img_data, circle) {
       var b = img_data.data[index + 2];
       var a = img_data.data[index + 3];
       debugger
-      if (r<127) {
+      if (r*a/255<127) {
         points_overlapping++;
       }
     }
